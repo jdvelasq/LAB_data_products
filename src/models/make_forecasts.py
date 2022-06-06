b92ba4936843fbb5a8ba74dbe023a7032389f294
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-06 06:35:29 -0500

    Create make_forecasts.py

---
+++ src/models/make_forecasts.py
@@ -0,0 +1,15 @@
+def make_forecasts():
+    """Construya los pronosticos con el modelo entrenado final.
+
+    Cree el archivo data_lake/business/forecasts/precios-diarios.csv. Este
+    archivo contiene tres columnas:
+
+    * La fecha.
+
+    * El precio promedio real de la electricidad.
+
+    * El pronóstico del precio promedio real.
+
+
+    """
+    raise NotImplementedError("Implementar esta función")
