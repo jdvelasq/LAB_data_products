03bf187bd6db40f27414524a69b4726ef1decce8
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-06 06:35:13 -0500

    Create create_data_lake.py

---
+++ src/data/create_data_lake.py
@@ -0,0 +1,24 @@
+def create_data_lake():
+    """Cree el data lake con sus capas.
+
+    Esta función debe crear la carpeta `data_lake` en la raiz del proyecto. El data lake contiene
+    las siguientes subcarpetas:
+
+    ```
+    .
+    |
+    \___ data_lake/
+         |___ landing/
+         |___ raw/
+         |___ cleansed/
+         \___ business/
+              |___ reports/
+              |    |___ figures/
+              |___ features/
+              |___ forecasts/
+
+    ```
+
+
+    """
+    raise NotImplementedError("Implementar esta función")
